e55cd752d0977e6d92c3743c3ac5158beea2db10
Cu63 <scube.str@gmail.com>
2022-12-24 13:36:54 +0300

    creating json creator

--- source/api_parse.py
+++ source/api_parse.py
@@ -1,4 +1,19 @@
 import requests
+import json
+
+
+def send_answer(cords: list, bags: list):
+    map_id = 'faf7ef78-41b3-4a36-8423-688a61929c08'
+    cords = [[(1, 2), (1, 3), (1, 4)]]
+    moves = []
+    ans ={'moves': moves}
+    for way in cords:
+        for point in way:
+            moves.append({"x": point[0], "y": point[1]})
+        moves.append({"x": 0, "y": 0})
+    ans = json.dumps(ans)
+    print(ans)
+
 
 
 def get_map():
@@ -13,9 +28,8 @@ def get_map():
 
 
 def _main():
-    map_ = get_map()
-    print(map_)
-
+    # map_ = get_map()
+    send_answer([], [])
 
 
 if __name__ == '__main__':
--- source/main.py
+++ source/main.py
@@ -1,4 +1,27 @@
 import json
+from api_parse import send_answer
+from PIL import Image, ImageDraw
+
+
+def draw_map(children, snow_a):
+    img = Image.new(mode="RGB", size=(10000, 10000), color=(255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    draw.ellipse((-20, -20, 20, 20), fill='red')
+    for s in snow_a:
+        x = s['x']
+        y = s['y']
+        r = s['r']
+        draw.ellipse((x-r, y-r, x+r, y+r), fill='blue')
+        print(s)
+    for c in children:
+        x = c['x']
+        y = c['y']
+        draw.rectangle((x-20, y-20, x+20, y+20), fill='green')
+        print(c)
+
+    img.show()
+    img.save('1.png')
+
 
 def get_data():
     data = dict()
@@ -11,6 +34,8 @@ def main():
     print('gifts:', gifts)
     print('children:', children)
     print('snowAreas:', snowAreas)
+    draw_map(children, snowAreas)
+    send_answer()
 
 
 if __name__ == '__main__':
